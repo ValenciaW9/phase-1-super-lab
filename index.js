d6ae9aadfa1f3b8736a94f0234491275cfdd063f
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-07 14:39:37 -0400

    phase-1-super-lab
    phase-1-super-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your classes here
+class Tree {
+    constructor(species) {
+        this.species = species;
+    }
+    Static(defintion) {
+        return `A tree is a perennial plant with leaves.`;
+      }
+    }
+
+  class Evergreen extends Tree {
+    constructor(species, name) {
+      super(species);
+      this.name = name;
+    }
+    Static(defintion) {
+      return '${super.definition ()} Evergreens keep  their leaves all year round.';
+    }
+  }
+
+const oak = new Decididuous('Oak', 'Common Oak');
+console.log(oak.definition()); // A tree is a  perennial plant with an alongatted stem, or trunk , supporting  branches and leaves annually,
+
+const pine = new Evergreen('Pine', 'Scots Pine');
+console.log(pine.definition()); // A tree is a perennial plant  with an eloggated  stem, or trunk, supporting  branches and leaves. Evergreens kee[ their  leaves all year  round.]
+
+    
